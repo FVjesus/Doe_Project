0b8a144e942865b47ac10d11cab19d31c0a1b975
Fabrício <fabriciovellozo@gmail.com>
2020-03-22 09:36:35 -0300

    Add logic for donors
    adds logic for the display of the latest donors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,37 @@
 const express = require("express");
-const server =  express();
+const server = express();
 
 const nunjucks = require("nunjucks");
 
 server.use(express.static('../web/public'));
-server.use(express.static('../web/assets'));
 
-nunjucks.configure("../web",{
-    express: server
+nunjucks.configure("../web", {
+    express: server,
+    noCache: true,
 });
 
-server.get("/", function(require, response){
-    return response.render("index.html");
+
+const donors = [
+    {
+        name: "Fabrício Veloso",
+        blood: "AB+"
+    },
+    {
+        name: "Erick Joho",
+        blood: "B-"
+    },
+    {
+        name: "Kadmus Krigem",
+        blood: "O+"
+    },
+    {
+        name: "Homer Simpson",
+        blood: "AB-"
+    }
+]
+
+server.get("/", function (require, response) {
+    return response.render("index.html", { donors });
 })
 
 
